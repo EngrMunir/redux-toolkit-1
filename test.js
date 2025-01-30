bffdd203c940bd62e7f37b0519d0cfb44aae17aa
munir <smsirajulmonir@gmail.com>
2025-01-30 16:35:57 +0600

    function currying explored

--- test.js
+++ test.js
@@ -1,15 +1,40 @@
+// mutation handle
+// const employee ={ 
+//     name: "Mir",
+//     address:{ country: "Bangladesh", city:"Dhaka"}
+// };
 
-const employee ={ 
-    name: "Mir",
-    address:{ country: "Bangladesh", city:"Dhaka"}
-};
+// const employee2 = {
+//     ...employee, 
+//     name:"munir",
+//     address:{...employee.address, city:"Chittagong"}
+// };
 
-const employee2 = {
-    ...employee, 
-    name:"munir",
-    address:{...employee.address, city:"Chittagong"}
-};
+// console.log(employee);
+// console.log(employee2);
 
+// function currying
 
-console.log(employee);
-console.log(employee2);
+// normal function
+// const add = (a,b) => a+b;
+
+const add =(a)=>(b)=>a+b;
+
+// console.log(add(5)(5));
+
+function add2(a){
+    return function (b){
+        return a+b;
+    }
+}
+
+console.log(add2(2)(3))
+
+const totalPrice =(discount) =>(amount) => amount-amount*discount;
+
+const withDiscount = totalPrice(0.3);
+
+console.log(withDiscount(100));
+console.log(withDiscount(300));
+console.log(withDiscount(400));
+console.log(withDiscount(500));
